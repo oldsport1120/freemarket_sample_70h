e577ee35df2ba1e568de078ef78216ebb4b641ac
HI <h.ito19821120@gmail.com>
2020-05-19 23:44:34 +0900

    Update 20200518171824_create_products.rb

--- db/migrate/20200518171824_create_products.rb
+++ db/migrate/20200518171824_create_products.rb
@@ -10,7 +10,7 @@ class CreateProducts < ActiveRecord::Migration[5.2]
       t.string :shipping_place, null:false
       t.integer :shipping_period, null:false
       t.integer :user_id, foreign_key:true
-      t.integer :category, foreign_key:true
+      t.integer :category_id, foreign_key:true
 
       t.timestamps
     end
