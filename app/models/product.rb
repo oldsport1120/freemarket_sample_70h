b8ad9cb1f8ccb11e5944414ccbac1d478590f38b
sh0327akg <shota.gk0327m.tf@gmail.com>
2020-05-24 22:48:25 +0900

    モデルの編集

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,6 @@ class Product < ApplicationRecord
   # active_hashのアソシエーション
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
-
   has_many :pictures, dependent: :destroy
+  accepts_nested_attributes_for :pictures
 end
