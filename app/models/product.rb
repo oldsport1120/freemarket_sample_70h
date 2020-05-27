555fbde95ac6d88466449c08693e1817a38df726
HI <h.ito19821120@gmail.com>
2020-05-27 15:38:26 +0900

    画像投稿・バリデーションの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,8 @@ class Product < ApplicationRecord
 
   has_many :pictures, dependent: :destroy
   accepts_nested_attributes_for :pictures
+
+  validates :products_name, presence: true, length: { maximum: 40 }
+  validates :descreption, presence: true, length: { maximum: 1000 }
+  validates :price, :product_condition, :shipment_fee, :shipping_place, presence: true
 end
