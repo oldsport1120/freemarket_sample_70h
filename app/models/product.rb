a950cbf5da2e1ef9fabd82c647dbf899eee70394
HI <h.ito19821120@gmail.com>
2020-05-30 15:15:43 +0900

    jqueryの導入

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,10 @@ class Product < ApplicationRecord
   # active_hashのアソシエーション
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
+  
   has_many :pictures, dependent: :destroy
   accepts_nested_attributes_for :pictures
+
   validates :products_name, presence: true, length: { maximum: 40 }
   validates :descreption, presence: true, length: { maximum: 1000 }
   validates :price, :product_condition, :shipment_fee, :shipping_place, presence: true
