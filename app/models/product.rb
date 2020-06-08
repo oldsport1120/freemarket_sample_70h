2867f52724ecccc730ab81f483f9d3884f449924
HI <h.ito19821120@gmail.com>
2020-06-09 02:10:53 +0900

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,9 @@ class Product < ApplicationRecord
   has_many :pictures, dependent: :destroy
   accepts_nested_attributes_for :pictures
 
+  belongs_to :seller, class_name: 'User', foreign_key: 'seller_id'
+  belongs_to :buyer, class_name: 'User', foreign_key: 'buyer_id'
+
   validates :products_name, presence: true, length: { maximum: 40 }
   validates :descreption, presence: true, length: { maximum: 1000 }
   validates :price, :product_condition, :shipment_fee, :shipping_place, presence: true
