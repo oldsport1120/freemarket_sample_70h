c33a26480378b8ad36989d23fb3ca4ca1faf1ba0
HI <h.ito19821120@gmail.com>
2020-06-23 21:41:14 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -43,10 +43,10 @@ ActiveRecord::Schema.define(version: 2020_06_12_175622) do
     t.integer "shipping_period", null: false
     t.bigint "user_id"
     t.integer "category_id"
-    t.integer "buyer_id"
     t.integer "sale_status", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "buyer_id"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
